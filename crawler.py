3285cad55163a9a10d0101f45bce7a249c5d665f
Sascha Peter <sascha.peter@stickyboard.co.uk>
2015-11-01 22:06:46 +0000

    new: dev: Add element click and country select to crawler

--- crawler.py
+++ crawler.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 """This file contains the selenium web crawler and it's logic
 
 @author: Sascha Peter <sascha.o.peter@gmail.com>
@@ -6,6 +8,7 @@
 """
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 class SeleniumCrawler(object):
@@ -13,12 +16,26 @@ class SeleniumCrawler(object):
 
     def __init__(self):
         self.driver = webdriver.Firefox()
+        self.country_list = ["Canada"]
 
     def test(self):
-        self.driver.get("http://international.o2.co.uk/internationaltariffs/calling_abroad_from_uk")
+        self.driver.get("http://international.o2.co.uk/internationaltariffs/" +
+                        "calling_abroad_from_uk")
         print(self.driver.title)
+
+    def get_tariff_information(self, country):
+        """Finds the country input element and searches for the given
+        country
+        """
+        input_element = self.driver.find_element_by_id("countryName")
+        input_element.click()
+        input_element.send_keys(country)
+        input_element.send_keys(Keys.RETURN)
+        monthly_element = self.driver.find_element_by_id("paymonthly")
+        monthly_element.click()
 
 
 if __name__ == '__main__':
     crawler = SeleniumCrawler()
     crawler.test()
+    crawler.get_tariff_information("Canada")
